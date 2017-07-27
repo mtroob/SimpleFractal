448348d8b8f4e81347c722b03202117a4d01cab7
Michael Troob <mtroob@gmail.com>
2017-07-27 19:17:35 +0300

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jul 27, 2017
+ *      Author: michael
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+	cout << "Fractal generation software" << endl;
+}
+
